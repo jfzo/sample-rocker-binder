436ebd1f61d7a042db7891169257b87d30ae4b4c
Juan Zamora O <jfzo@users.noreply.github.com>
2024-07-04 10:04:27 -0400

    Update install.R

--- install.R
+++ install.R
@@ -1,5 +1,5 @@
 # Machine Learning Libraries
-install.packages(c("dplyr","shiny","foreach","microbenchmark","parallel","runit","arules","arulesSequences","neuralnet","RSNNS","AUC","sprint","recommenderlab","acepack","addinexamples","clv","cubature","dtw","Formula","git2r","googleVis","gridExtra","gsubfn","hash","Hmisc","ifultools","latticeExtra","locpol","longitudinalData","lubridate","miniUI","misc3d","mvtsplot","np","openssl","packrat","pdc","PKI","rsconnect","splus2R","sqldf","TaoTeProgramming","TraMineR","TSclust","withr","wmtsa"), repos="http://cran.us.r-project.org", dependencies=TRUE)
+install.packages(c("dplyr","shiny","foreach","parallel","runit"), repos="http://cran.us.r-project.org", dependencies=TRUE)
 
 # Install R Jupyter Kernel
 install.packages(c("repr", "IRdisplay", "crayon", "pbdZMQ", "devtools"),repos="http://cran.us.r-project.org", dependencies=TRUE)
